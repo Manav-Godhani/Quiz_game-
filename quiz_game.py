4c207f17a3967cda9152a4c190af900809c516ca
mnv-godhani <godhanik87@gmail.com>
2025-12-04 09:54:25 +0530

    questions Added

--- quiz_game.py
+++ quiz_game.py
@@ -1,2 +1,27 @@
 # quiz game - mnv_godhani 
 
+
+
+# ----- Quiz Questions -----
+questions = [
+    {
+        "question": "What is the capital of India?",
+        "options": ["A) Mumbai", "B) Delhi", "C) Kolkata", "D) Chennai"],
+        "answer": "B"
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["A) Earth", "B) Jupiter", "C) Mars", "D) Venus"],
+        "answer": "C"
+    },
+    {
+        "question": "What is 5 + 7?",
+        "options": ["A) 10", "B) 12", "C) 14", "D) 16"],
+        "answer": "B"
+    },
+    {
+        "question": "Who created Python language?",
+        "options": ["A) James Gosling", "B) Guido van Rossum", "C) Elon Musk", "D) Bill Gates"],
+        "answer": "B"
+    }
+]
